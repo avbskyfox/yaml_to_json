f93f60b77d80032fba511c0e294bdcd5621f6c88
Alex Bubnov <avb_it@outlook.com>
2022-11-02 19:56:48 +0500

    yaml loader middle

--- main.py
+++ main.py
@@ -22,13 +22,20 @@ data = {
                 'address': 'St.Ptersburg',
                 'class': 32,
                 'discipline': 'Programming'
-            }
-        ]
+            },
+            'asdasd'
+        ],
+    'wensday': 'qsadasd',
+    123: 213
 }
 
 
+with open('test_shcedule.yml') as f:
+    string = f.read()
+
 def main():
-    print(YAML.dump_string(data, 2))
+    # print(YAML.dump_string(data, 2))
+    print(YAML.load_string(string))
 
 
 if __name__ == '__main__':
--- yaml_parser.py
+++ yaml_parser.py
@@ -2,21 +2,25 @@ class TypeIsNotSerializable(Exception):
     pass
 
 
+class BadSyntax(Exception):
+    pass
+
+
 class Dumper:
     def __init__(self, indent):
         self.indent = indent
-        self.__current_indent = 0
+        self.__current_indent_count = 0
 
-    def get_indent(self):
-        if self.__current_indent == 0:
+    def __current_indent(self):
+        if self.__current_indent_count == 0:
             return ''
-        return ' ' * self.__current_indent
+        return ' ' * self.__current_indent_count
 
     def __add_indent(self):
-        self.__current_indent += self.indent
+        self.__current_indent_count += self.indent
 
     def __remove_indent(self):
-        self.__current_indent -= self.indent
+        self.__current_indent_count -= self.indent
 
     @staticmethod
     def __key(val):
@@ -31,10 +35,10 @@ class Dumper:
         return str(val)
 
     def __scalar_array_item(self, val):
-        return f'{self.get_indent()}- {self.process(val)}\n'
+        return f'{self.__current_indent()}- {self.process(val)}\n'
 
     def __object_or_array_item(self, val):
-        result = f'{self.get_indent()}-\n'
+        result = f'{self.__current_indent()}-\n'
         self.__add_indent()
         result += f'{self.process(val)}'
         self.__remove_indent()
@@ -48,7 +52,7 @@ class Dumper:
 
     def __array(self, val):
         if len(val) == 0:
-            return f'{self.__current_indent}-\n'
+            return f'{self.__current_indent_count}-\n'
         result_string = ''
         for item in val:
             result_string += self.__array_element(item)
@@ -56,9 +60,9 @@ class Dumper:
 
     def __key_value_element(self, key, value):
         if type(value) in [str, int, float]:
-            return f'{self.get_indent()}{key}: {self.process(value)}\n'
+            return f'{self.__current_indent()}{key}: {self.process(value)}\n'
         if type(value) in [list, dict]:
-            result = f'{self.get_indent()}{key}:'
+            result = f'{self.__current_indent()}{key}:'
             self.__add_indent()
             result += f'\n{self.process(value)}'
             self.__remove_indent()
@@ -83,11 +87,136 @@ class Dumper:
             raise TypeIsNotSerializable(f"can not serialize value of type {type(val)}")
 
 
-class YAML:
-    @staticmethod
-    def load_string(s: str):
-        pass
+class Loader:
+    def __init__(self, indent):
+        self.__indent = indent
+        self.__current_indent_count = 0
+
+    def __current_indent(self):
+        if self.__current_indent == 0:
+            return ''
+        return ' ' * self.__current_indent_count
+
+    def __remove_indent(self, string: str):
+        result = ''
+        for row in string.split('\n'):
+            result += f'{row.removeprefix(" " * self.__indent)}\n'
+        return result[0:-1]
 
     @staticmethod
-    def dump_string(data, indent=2, dumper_class=Dumper):
+    def __string(string: str):
+        return string[1: -1]
+
+    @staticmethod
+    def __number(string: str):
+        if string.find('.') > 0:
+            return float(string)
+        else:
+            return int(string)
+
+    def __key(self, string: str):
+        return self.__scalar_item(string)
+
+    def __key_value_element(self, string: str):
+        key = string[0:string.find(':')]
+        prevalue = string[string.find(':')+2:]
+        if prevalue.startswith('  ' * self.__indent):
+            value = self.__remove_indent(prevalue)
+        else:
+            value = prevalue
+        if self.__type(value) == 'object':
+            return key, self.__object_or_array_item(value)
+        if self.__type(value) == 'scalar':
+            return key, self.__scalar_item(value)
+
+    def __key_value_list(self, string: str):
+        result = []
+        rows = string.split('\n')
+        buffer = str()
+        for i in range(0, len(rows)):
+            buffer += f'{rows[i]}\n'
+            try:
+                if not (rows[i+1].startswith(' ' * self.__indent) or rows[i+1].startswith('-')):
+                    result.append(buffer[0:-1])
+                    buffer = str()
+            except IndexError:
+                result.append(buffer[0:-1])
+        return result
+
+    def __array_element(self, string: str):
+        if string.startswith('- '):
+            return self.__scalar_item(string.removeprefix('- ').strip('\n '))
+        if string.startswith('-\n'):
+            return self.__object_or_array_item(self.__remove_indent(string.removeprefix('-\n')))
+
+    def __array_element_list(self, string: str):
+        result = []
+        rows = string.split('\n')
+        buffer = str()
+        for i in range(0, len(rows)):
+            if i == 1:
+                continue
+            if rows[i].startswith('- '):
+                result.append(rows[i][2:])
+            buffer += f'{rows[i]}\n'
+            try:
+                if rows[i+1].startswith('-'):
+                    result.append(buffer[0:-1])
+                    buffer = str()
+            except IndexError:
+                result.append(buffer)
+        return result
+
+    def __array(self, string: str):
+        result = []
+        for element in self.__array_element_list(string):
+            result.append(self.__array_element(element))
+        return result
+
+    def __object(self, string: str):
+        result = {}
+        for key_value in self.__key_value_list(string):
+            key, value = self.__key_value_element(key_value)
+            result.update({key: value})
+        return result
+
+    def __scalar_item(self, string: str):
+        def is_float(string: str):
+            if string.find('.') > 0 and len(string.split('.')) == 2:
+                return True
+        if is_float(string) or string.isnumeric():
+            return self.__number(string)
+        else:
+            return self.__string(string)
+
+    def __object_or_array_item(self, string: str):
+        rows = string.split('\n')
+        if rows[0].startswith('- ') or rows[0] == '-':
+            return self.__array(string)
+        elif rows[0].find(':\n') > 0 or rows[0].find(': ') > 0:
+            return self.__object(string)
+        else:
+            raise BadSyntax
+
+    @staticmethod
+    def __type(string: str):
+        if len(string.split('\n')) > 1:
+            return 'object'
+        else:
+            return 'scalar'
+
+    def process(self, string: str):
+        if self.__type(string) == 'object':
+            return self.__object_or_array_item(string)
+        if self.__type(string) == 'scalar':
+            return self.__scalar_item(string)
+
+
+class YAML:
+    @staticmethod
+    def load_string(s: str, indent=4, loader_class=Loader):
+        return loader_class(indent=indent).process(s)
+
+    @staticmethod
+    def dump_string(data, indent=4, dumper_class=Dumper):
         return dumper_class(indent=indent).process(data)
